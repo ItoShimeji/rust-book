a558f2dd10770ef71b75d39d5b96d55689829be0
ItoShimeji <hamazakidaiti@gmail.com>
2026-05-07 00:52:19 +0900

    refactor: practice_event_router

--- c6/practice_event_router/src/main.rs
+++ c6/practice_event_router/src/main.rs
@@ -1,15 +1,6 @@
-struct Login {
-    user: String,
-}
-
-struct Message {
-    from: String,
-    text: String,
-}
-
 enum AppEvent {
-    Login(Login),
-    Message(Message),
+    Login { user: String },
+    Message { from: String, text: String },
     Warning(String),
     Logout,
     Noop,
@@ -31,13 +22,13 @@ fn main() {
     let mut state = RouterState::new();
 
     let events = [
-        AppEvent::Login(Login {
+        AppEvent::Login {
             user: "Aki".to_string(),
-        }),
-        AppEvent::Message(Message {
+        },
+        AppEvent::Message {
             from: "Aki".to_string(),
             text: "hello".to_string(),
-        }),
+        },
         AppEvent::Warning("disk almost full".to_string()),
         AppEvent::Logout,
         AppEvent::Noop,
@@ -51,23 +42,27 @@ fn main() {
 
 fn handle_event(state: &mut RouterState, event: AppEvent) -> String {
     match event {
-        AppEvent::Login(event) => {
-            let user = &event.user[..];
+        AppEvent::Login { user } => {
+            // ここは所有権エラーを起こさないように message を返すための処理と login user の登録処理が交互になっている
+            // move する前とした後はこのように順序を分離して書くパターンが Rust では必要なのか
+            // format! が借用を暗黙的に受け取るのはマクロの影響か？
             let message = format!("{user} is logged in");
-            state.logged_in_user = Some(event.user);
+
+            state.logged_in_user = Some(user);
             message
         }
-        AppEvent::Message(message) => {
-            if message.text.is_empty() {
-                format!("empty message from {}", message.from)
+        AppEvent::Message { from, text } => {
+            if text.is_empty() {
+                format!("empty message from {}", from)
             } else {
-                format!("{}: {}", message.from, message.text)
+                format!("{}: {}", from, text)
             }
         }
         AppEvent::Logout => {
-            if let Some(user) = &mut state.logged_in_user {
+            // take() によって、Some() を None にして取り出す
+            // Some の値の所有権が左辺に移る
+            if let Some(user) = state.logged_in_user.take() {
                 let message = format!("{user} is logged out");
-                state.logged_in_user = None;
                 message
             } else {
                 String::from("logout is failure because there is no logged in user")
