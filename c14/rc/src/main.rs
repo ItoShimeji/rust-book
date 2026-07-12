eeae0e087a092f9615b2554432aa691350e60010
ItoShimeji <hamazakidaiti@gmail.com>
2026-07-12 20:37:38 +0900

    ch15-04-rc

---
+++ c14/rc/src/main.rs
@@ -0,0 +1,29 @@
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
+}
+
+use crate::List::{Cons, Nil};
+use std::rc::Rc;
+
+fn main() {
+    {
+        let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+
+        // ここでの clone は ref count を increase させるだけの軽量な操作
+        let b = Cons(3, Rc::clone(&a));
+        let c = Cons(4, Rc::clone(&a));
+    }
+
+    {
+        let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+        println!("count after creating a = {}", Rc::strong_count(&a)); // weak_count も存在
+        let b = Cons(3, Rc::clone(&a));
+        println!("count after creating b = {}", Rc::strong_count(&a));
+        {
+            let c = Cons(4, Rc::clone(&a));
+            println!("count after creating c = {}", Rc::strong_count(&a));
+        }
+        println!("count after c goes out of scope = {}", Rc::strong_count(&a));
+    }
+}
